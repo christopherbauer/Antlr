0854107bfde2999dd0d5fceca2a27dc196ad5488
Christopher Bauer <chrisbauer.career@gmail.com>
2015-10-22 17:12:41 -0400

    First commit of Antlr A project that checks a directory and informs on ant-like filter syntax

---
+++ Antlr/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace Antlr
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            DataContext = new DesignMainWindowViewModel();
+            InitializeComponent();
+        }
+    }
+}
